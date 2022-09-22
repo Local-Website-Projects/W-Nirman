3cb97ac4ef8303b48f98f2af64f00af9e77e2d15
Mugdho <saha2171515@stud.kuet.ac.bd>
2022-09-22 17:38:32 +0600

    22/09/22

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -4,5 +4,5 @@ if (!isset($_SESSION)){
 }
 $con=mysqli_connect("localhost","root","","nirman") or die("Could not Connect My Sql");
 
-/*$con=mysqli_connect("premium11","biplgmwr_review","{vxs&Ce(^Jf0","biplgmwr_review") or die("Could not Connect My Sql");*/
+/*$con=mysqli_connect("premium11","biplgmwr_nirman","JA#8i9rYhH)f","biplgmwr_nirman") or die("Could not Connect My Sql");*/
 
